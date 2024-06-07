2ea14291cef0f38bc660356fee8a689aa68d20e9
bwyoungy <youngyaacov@gmail.com>
2024-06-07 17:07:53 +0300

    update frontend to use Redux State

---
+++ frontend/src/Redux/VacationState.ts
@@ -0,0 +1,49 @@
+import { createStore } from "redux";
+import VacationModel from "../Models/VacationModel";
+
+// Global state
+export class VacationState {
+    public vacations: VacationModel[] = [];
+}
+
+// Action types
+export const VacationsActionType = {
+    GetVacations: "GetVacations",
+    AddVacation: "AddVacation",
+    UpdateVacation: "UpdateVacation",
+    DeleteVacation: "DeleteVacation"
+}
+
+// Action
+export interface VacationsAction {
+    type: string,
+    payload: any
+}
+
+// Reducer
+export function vacationsReducer(currentState = new VacationState(), action:VacationsAction) : VacationState {
+    const dupState = {...currentState}; // copy by refrence
+
+    switch (action.type) {
+        case VacationsActionType.GetVacations:
+            dupState.vacations = action.payload;
+            break;
+    
+        case VacationsActionType.AddVacation:
+            dupState.vacations.push(action.payload);
+            break;
+    
+        case VacationsActionType.UpdateVacation:
+            dupState.vacations[dupState.vacations.findIndex(v => v.vacationID === action.payload.vacationID)] = action.payload;
+            break;
+    
+        case VacationsActionType.DeleteVacation:
+            dupState.vacations.splice(dupState.vacations.findIndex(v => v.vacationID === action.payload), 1);
+            break;
+    }
+
+    return dupState;
+}
+
+// Store
+export const vacationsStore = createStore(vacationsReducer);
--- frontend/src/Services/VacationsService.ts
+++ frontend/src/Services/VacationsService.ts
@@ -1,29 +1,48 @@
 import axios from "axios";
 import VacationModel from "../Models/VacationModel";
 import appConfig from "../Utils/Config";
+import { VacationsActionType, vacationsStore } from "../Redux/VacationState";
 
 class VacationService {
     public async getAllVacations():Promise<VacationModel[]> {
-        const response = await axios.get<VacationModel[]>(appConfig.vacationsUrl);
-        return response.data;
+        let vacations = vacationsStore.getState().vacations;
+
+        if (vacations.length === 0 ) {
+            const response = await axios.get<VacationModel[]>(appConfig.vacationsUrl);
+            vacationsStore.dispatch({type: VacationsActionType.GetVacations, payload: response.data});
+        }
+        
+        return vacations;
     }
 
     public async getVacationById(id:number):Promise<VacationModel> {
-        const response = await axios.get<VacationModel>(appConfig.vacationsUrl + id);
+        let vacations = vacationsStore.getState().vacations;
+        let vacation = vacations.find(v => v.vacationID === id);
+
+        if(!vacation) {
+            const response = await axios.get<VacationModel>(appConfig.vacationsUrl + id);
+            vacation = response.data;
+        }
         
-        return response.data;
+        return vacation;
     }
 
     public async addVacation(vacation:VacationModel):Promise<void> {
         const response = await axios.post<VacationModel>(appConfig.vacationsUrl, vacation);
+
+        vacationsStore.dispatch({type: VacationsActionType.AddVacation, payload:response.data});
     }
 
     public async updateVacation(vacation:VacationModel): Promise<void> {
         const response = await axios.put<VacationModel>(appConfig.vacationsUrl + vacation.vacationID, vacation);
+
+        vacationsStore.dispatch({type: VacationsActionType.UpdateVacation, payload:response.data});
     }
 
     public async deleteVacation(id:number): Promise<void> {
         await axios.delete<void>(appConfig.vacationsUrl + id);
+
+        vacationsStore.dispatch({type: VacationsActionType.DeleteVacation, payload: id});
     }
 }
 

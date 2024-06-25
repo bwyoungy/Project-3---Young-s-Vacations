5f0928bc0a5d517b2de7f60d0025e354f90f42e7
bwyoungy <youngyaacov@gmail.com>
2024-06-25 05:35:13 +0300

    add home page description

--- frontend/src/Components/HomeArea/Home/Home.tsx
+++ frontend/src/Components/HomeArea/Home/Home.tsx
@@ -3,7 +3,11 @@ import "./Home.css";
 function Home(): JSX.Element {
     return (
         <div className="Home">
-			Welcome to Young's Vacations!
+			<h1>Welcome to Young's Vacations!</h1>
+            <h2>The travelling sensation gripping the nation!</h2>
+            <p>Use this lovely website to check out all the great up and coming vacations!</p>
+            <p>For each vacation you can see it's destination, description, image, price, dates, and followers</p>
+            <p>Follow your favourite vacations!</p>
         </div>
     );
 }
